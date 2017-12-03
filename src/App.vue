d739edf5afb3a38ac2be079addb0dde368d6eb66
Daniel Simmonds <simmondsdt@gmail.com>
2017-12-03 14:41:27 -0700

    add base components for chart and todo list

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
     <br />
-    <div class="container text-center">
+    <div id="title" class="container text-center">
       <h2>My Todo List</h2>
     </div>
     <br />
     <div class="row">
       <div class="col-sm-6 text-center">
         Chart will go here
+        <comparison-chart></comparison-chart>
       </div>
       <div class="col-sm-6 text-center">
         Todos will go here
+        <todo-list></todo-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ComparisonChart from './components/ComparisonChart';
+import TodoList from './components/TodoList';
+
 export default {
   name: 'app',
   components: {
+    ComparisonChart,
+    TodoList,
   },
 };
 </script>
---
+++ src/components/ComparisonChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Comparison Chart</p>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+</style>
