6dfa2286c20299df22a549f4b85ba9d9b32129db
Daniel Simmonds <simmondsdt@gmail.com>
2017-12-06 18:38:30 -0700

    adjust graph to watch for data change

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,38 +1,51 @@
-<template>
-  <div>
-    <p>Completed Tasks: {{todos.filter(todo => {return todo.completed === true}).length}}</p>
-    <p>Pending Tasks: {{todos.filter(todo => {return todo.completed === false}).length}}</p>
-  </div>
-</template>
-
 <script>
-// import { Bar } from 'vue-chartjs';
+import { Bar } from 'vue-chartjs';
 
 export default {
   props: ['todos'],
-//   extends: Bar,
-//   mounted() {
-//     // eslint-disable-next-line
-//   this.getCompleteTodos = this.todos.filter(todo => { return todo.completed === true; }).length;
-//     // eslint-disable-next-line
-//   this.getPendingTodos = this.todos.filter(todo => { return todo.completed === false; }).length;
-
-//     this.renderChart({
-//       labels: ['', ''],
-//       datasets: [
-//         {
-//           label: 'Completed',
-//           borderColor: '#007bff',
-//           backgroundColor: '#007bff',
-//           data: [this.getCompleteTodos, null],
-//         }, {
-//           label: 'Pending',
-//           borderColor: '#dc3545',
-//           backgroundColor: '#dc3545',
-//           data: [null, this.getPendingTodos],
-//         },
-//       ],
-//     }, { responsive: true, maintainAspectRatio: false });
-//   },
+  extends: Bar,
+  watch: {
+    todos: {
+      handler() {
+        this.updateChart();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    updateChart() {
+      this.renderChart({
+        labels: ['', ''],
+        datasets: [
+          {
+            label: 'Completed',
+            borderColor: '#007bff',
+            backgroundColor: '#007bff',
+            data: [
+              // eslint-disable-next-line
+              this.todos.filter(todo => {
+                return todo.completed === true;
+              }).length,
+              null,
+            ],
+          }, {
+            label: 'Pending',
+            borderColor: '#dc3545',
+            backgroundColor: '#dc3545',
+            data: [
+              null,
+              // eslint-disable-next-line
+              this.todos.filter(todo => {
+                return todo.completed === false;
+              }).length,
+            ],
+          },
+        ],
+      }, { responsive: true, maintainAspectRatio: true });
+    },
+  },
+  mounted() {
+    this.updateChart();
+  },
 };
 </script>
